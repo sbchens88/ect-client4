c679ff0903c568433ff21fd2c3c9a7f0210f1d1d
Support eradani <support-eradani@gmail.com>
2022-07-10 12:31:26 +0300

    Manual API edit by: Support eradani

--- src/routes/asdasd/index.ts
+++ src/routes/asdasd/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import mountASDWEDF from './asdwedf';
 import mountASDSADW from './asdsadw';
 
-console.log('routes index')
+console.log('routes index');
 
 export default function mountASDASD(router: Router) {
     const asdwedf = Router();
